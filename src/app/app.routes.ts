61c09878f7ec1aa9a60fb834a77f49e594cf3977
Rafael Ferrari <rafaelferrarifilho1234@gmail.com>
2025-04-25 17:11:24 -0300

    Melhorias no início, home, sobre e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,12 @@ import { LoginComponent } from './pages/autenticacao/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { TipoServicoListComponent } from './pages/tipo-servico/list/tipo-servico-list.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 export const routes: Routes = [
-
   { path: 'login', component: LoginComponent },
-  { path: 'tipo-servico/listar', component: TipoServicoListComponent}, //, canActivate: [AuthGuard] 
+  { path: 'tipo-servico/listar', component: TipoServicoListComponent }, //, canActivate: [AuthGuard] 
   { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: "/home" }
-
-
+  { path: 'sobre', component: SobreComponent }, //, canActivate: [AuthGuard]
+  { path: '**', redirectTo: "/home" } // Mova esta rota para o final
 ];
