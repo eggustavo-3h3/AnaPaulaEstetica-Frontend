e40cbb9bdda08f14220ed1931858029058c74fad
Rafael Ferrari <rafaelferrarifilho1234@gmail.com>
2025-05-06 01:25:54 -0300

    Melhorias no sobre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AuthGuard } from './guards/auth.guard';
 import { TipoServicoListComponent } from './pages/tipo-servico/list/tipo-servico-list.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { AgendamentoComponent } from './pages/agendamento/agendamento.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'tipo-servico/listar', component: TipoServicoListComponent }, //, canActivate: [AuthGuard] 
   { path: 'home', component: HomeComponent },
   { path: 'sobre', component: SobreComponent }, //, canActivate: [AuthGuard]
+  { path: 'agendamento', component: AgendamentoComponent },
   { path: '**', redirectTo: "/home" } // Mova esta rota para o final
 ];
