2d67fd9bc17737632468cb776da20b84abc4848b
mirela <telesmirela552@gmail.com>
2025-05-21 21:17:45 -0300

    CriaçaoCategoria

---
+++ src/app/pages/categoria-adicionar/categoria-adicionar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-adicionar',
+  imports: [],
+  templateUrl: './categoria-adicionar.component.html',
+  styleUrl: './categoria-adicionar.component.css'
+})
+export class CategoriaAdicionarComponent {
+
+}
