c3533f68d0b01575331a64ff0ad8e60f56743aba
mirela <telesmirela552@gmail.com>
2025-05-21 21:31:22 -0300

    Atualizado

---
+++ src/app/pages/categoria-atualizar/categoria-atualizar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-atualizar',
+  imports: [],
+  templateUrl: './categoria-atualizar.component.html',
+  styleUrl: './categoria-atualizar.component.css'
+})
+export class CategoriaAtualizarComponent {
+
+}
