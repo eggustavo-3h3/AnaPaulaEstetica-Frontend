4f793c6b70acdddaa79499f9968e9bcad3115e66
Rafael Ferrari <rafaelferrarifilho1234@gmail.com>
2025-05-29 01:04:35 -0300

    Criação de telas

--- src/app/pages/agendamento/agendamento.component.ts
+++ src/app/pages/agendamento/agendamento.component.ts
@@ -1,12 +1,19 @@
-import {Component, model} from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { Component, model } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-agendamento',
   standalone: true,
-  imports: [MatCardModule, MatDatepickerModule],
+  imports: [
+    MatCardModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './agendamento.component.html',
   styleUrl: './agendamento.component.css'
