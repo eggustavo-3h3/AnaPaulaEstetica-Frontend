752415387ea777ccd69249f111e75a79f59ff8d5
Eduardo Gustavo Gonçalves <eggustavo+3h3@gmail.com>
2025-06-10 00:56:08 -0300

    fix: update apiUrl in environment files for consistent API endpoint usage

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://anapaulaestetica-api.tccnapratica.com.br'
-    apiUrl: 'https://localhost:7200' 
+    apiUrl: 'https://anapaulaestetica-api.tccnapratica.com.br'
+    //apiUrl: 'https://localhost:7200' 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://anapaulaestetica-api.tccnapratica.com.br'
-    apiUrl: 'https://localhost:7200' 
+    apiUrl: 'https://anapaulaestetica-api.tccnapratica.com.br'
+    //apiUrl: 'https://localhost:7200' 
 };
